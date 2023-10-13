aeb106e77d5577d3d0cde54a2e53aa27a36df46b
Elybourland <JEBourland@yahoo.com>
2023-10-12 19:54:59 -0700

    data fetching

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,53 @@
       x: {{ x }}
       y: {{ y }}
     </div>
+    <div>
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.name }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li v-for="user in users2" :key="user.id">
+          {{ user.username }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      <ul>
+        <li v-for="user in users3" :key="user.id">
+          {{ user.email }}
+        </li>
+      </ul>
+    </div>
+    <div>
+      {{ user.name }}
+    </div>
   </div>
 </template>
 
 <script setup>
   const {x, y} = useMouse()
+
+  // client side fetching
+  const users = ref([])
+
+  onMounted(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => users.value = data)
+  })
+
+  // server side fetching - better for SEO
+  const { data: users2 } = await useAsyncData('users2', () => $fetch('https://jsonplaceholder.typicode.com/users'))
+
+  // short hand server side fetching - better for SEO
+  const { data: users3 } = await useFetch('https://jsonplaceholder.typicode.com/users')
+
+  // short hand server side fetching - with the pick option ( for decreasing payload size)
+  const { data: user } = await useFetch('https://jsonplaceholder.typicode.com/users/1', { pick: ['id', 'name', 'email'] })
 </script>
 
 <style scoped>
